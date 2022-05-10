f776c9489104c6426eb8545ff7d1465d910770eb
21059546 <105166816+21059546@users.noreply.github.com>
2022-05-10 10:26:37 +0100

    Add files via upload

---
+++ Unity_Global simulation/nature spreading/SimulateButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulateButton : MonoBehaviour
+{
+
+	public GameObject PlayImage;
+	public GameObject StopImage;
+	public Text Text;
+
+	public void SetIsRunning(bool isRunning)
+	{
+		PlayImage.SetActive(!isRunning);
+		StopImage.SetActive(isRunning);
+
+		Text.text = isRunning ? "STOP" : "SIMULATE";
+	}
+}
